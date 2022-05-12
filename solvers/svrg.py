8b5c604b14d62fb83a74d04d6a8e776c77c5cf81
Thomas Moreau <thomas.moreau.2010@gmail.com>
2022-05-13 00:35:42 +0200

    Apply suggestions from code review

--- solvers/svrg.py
+++ solvers/svrg.py
@@ -26,19 +26,19 @@ class Solver(BaseSolver):
         ]
     }
 
-    def set_objective(self, X, y, lmbd):
-        self.X, self.y, self.lmbd = X, y, lmbd
-        # Reponse vector used for tick logreg must be {1.0, -1.0}
-        self.y[y == 0] = -1.0
-        self.clf = SVRG(tol=1e-16, verbose=False, step_type=self.step_type)
+    def set_objective(self, X, y, lmbd, fit_intercept):
+        model = ModelLogReg(fit_intercept=fit_intercept).fit(X, y)
+        # Scaled with 1/n_samples as logistic objective is taken with mean
+        prox = ProxL2Sq(lmbd / X.shape[0])
+        self.clf = SVRG(
+            tol=1e-16, verbose=False, step_type=self.step_type,
+            record_every=int(1e8)
+        )
+        self.clf.set_model(model).set_prox(prox)
 
     def run(self, n_iter):
-        model = ModelLogReg(fit_intercept=False).fit(self.X, self.y)
-        # Scaled with 1/n_samples as logistic objective is taken with mean
-        prox = ProxL2Sq(self.lmbd / self.X.shape[0])
         optimal_step_size = 1 / model.get_lip_max()
         self.clf.max_iter = n_iter
-        self.clf.set_model(model).set_prox(prox)
         self.coef_ = self.clf.solve(step=optimal_step_size)
         
     def get_result(self):
